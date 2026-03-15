15691161c26ee8c5ba240084853c8402cc90a7a2
anpi0316 <anton.piatkun@netcracker.com>
2026-03-15 18:48:28 +0300

    Enable multiple answers for poll

--- bot.py
+++ bot.py
@@ -62,9 +62,9 @@ async def send_daily_poll(context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_poll(
             chat_id=CHAT_ID,
             question="Сегодня играем в WZ?",
-	    options=["19:30", "20:00", "Нет"],
+	    options=["✅ 19:30", "✅ 20:00", "❌ Нет"],
             is_anonymous=False,
-            allows_multiple_answers=False,
+            allows_multiple_answers=True,
         )
         logger.info("✅ Опрос успешно отправлен в чат %s", CHAT_ID)
     except Exception as e:
@@ -82,9 +82,9 @@ async def poll_now(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         await update.message.reply_poll(
             question="Сегодня играем в WZ?",
-            options=["19:30", "20:00", "Нет"],
+            options=["✅ 19:30", "✅ 20:00", "❌ Нет"],
             is_anonymous=False,
-            allows_multiple_answers=False,
+            allows_multiple_answers=True,
         )
     except Exception as e:
         await update.message.reply_text(f"Ошибка: {e}")
